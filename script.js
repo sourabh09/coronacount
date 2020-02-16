d42bc89fa1460c56b3d2c136ff63011da7c466fc
sourabh09 <qa.sourabh@gmail.com>
2020-02-16 19:14:46 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+
+    function getStatus() {
+
+        $.ajax({
+            url: 'https://coronavirus-tracker-api.herokuapp.com/all',
+            
+            success: function(data) {
+
+              console.log(data);
+              $('.root').html('');
+             $('.root').append('<div class="number"><h3>Confirmed</h3><span class="count">'+data.latest.confirmed+'</span></div>'); 
+             $('.root').append('<div class="number"><h3>Deaths</h3><span class="count">'+data.latest.deaths+'</span></div>');
+             $('.root').append('<div class="number"><h3>Recovered</h3><span class="count">'+data.latest.recovered+'</span></div>');
+             $('.refreshbutton').css("display","block");
+
+          $('.count').each(function () {
+        $(this).prop('Counter',0).animate({
+            Counter: $(this).text()
+        }, {
+          duration: 3000,
+          easing: 'swing',
+          step: function (now) {
+              $(this).text(Math.ceil(now));
+                }
+            });
+        });
+                //process the JSON data etc
+        }
+
+
+        })
+
+    }
+
+// $( document ).ready(function() {
+    
+//   $('.count').each(function () {
+//     $(this).prop('Counter',0).animate({
+//         Counter: $(this).text()
+//     }, {
+//         duration: 4000,
+//         easing: 'swing',
+//         step: function (now) {
+//             $(this).text(Math.ceil(now));
+//         }
+//     });
+// });
+    
+// });
